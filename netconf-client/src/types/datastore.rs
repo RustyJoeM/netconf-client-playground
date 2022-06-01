8be07ba459760c67a5955bb594cfba65873b1422
Jozef Miklos <rusty.joe.mik@gmail.com>
2022-06-01 07:57:28 +0200

    add NMDA datastores - operational, intended

--- netconf-client/src/types/datastore.rs
+++ netconf-client/src/types/datastore.rs
@@ -17,10 +17,14 @@ pub enum Datastore {
     /// Startup configuration loaded by the device when it boots.
     /// Usage of this datastore is typically conditioned with server side support of `:startup` capability.
     Startup,
+    /// A configuration datastore holding the complete intended configuration of the device. (RFC - 8342, [section 5.1.4](https://datatracker.ietf.org/doc/html/rfc8342#section-5.1.4))
+    Intended,
+    /// A datastore holding the complete operational state of the device. (RFC - 8342, [section 5.3](https://datatracker.ietf.org/doc/html/rfc8342#section-5.3))
+    Operational,
     /// A "fallback" datastore for currently non-standardized or lib-unsupported datastores.
     ///
     /// (NOT for URL string - there is standalone type for targets/sources of URL origin - [`crate::types::ConfigWaypoint`])
-    Other(String), // TODO - fix unknown datastore ids & problem with extra <other> wrapper in serialization
+    Other(String),
 }
 
 impl std::fmt::Display for Datastore {
@@ -29,6 +33,8 @@ impl std::fmt::Display for Datastore {
             Datastore::Running => "running",
             Datastore::Candidate => "candidate",
             Datastore::Startup => "startup",
+            Datastore::Intended => "intended",
+            Datastore::Operational => "operational",
             Datastore::Other(other) => other,
         };
         f.write_str(ptr)
@@ -43,6 +49,8 @@ impl FromStr for Datastore {
             "running" => Datastore::Running,
             "candidate" => Datastore::Candidate,
             "startup" => Datastore::Startup,
+            "intended" => Datastore::Intended,
+            "operational" => Datastore::Operational,
             s => Datastore::Other(s.to_string()),
         })
     }
@@ -50,7 +58,8 @@ impl FromStr for Datastore {
 
 impl Datastore {
     /// Provides slice of all the standardized datastore's names.
+    // TODO - make capability conditioned?
     pub fn values() -> Vec<&'static str> {
-        vec!["running", "candidate", "startup"]
+        vec!["running", "candidate", "startup", "intended", "operational"]
     }
 }
